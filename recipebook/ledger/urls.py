f66714907b0dbd19c90bf4435f3f69cfbbf35e57
Kayin Miranda <kayin.edianne.miranda@student.ateneo.edu>
2026-02-09 21:27:17 +0800

    workingsite

---
+++ recipebook/ledger/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from .views import recipe_list, recipe1, recipe2
+
+urlpatterns = [
+    path('recipes/list', recipe_list, name='recipe_list'),
+    path('recipe/1', recipe1, name='recipe_1'),
+    path('recipe/2', recipe2, name='recipe_2'),
+]
+
+app_name = "ledger"
