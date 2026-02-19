d9aefeb4e0991e05b20d02eb9f3a068099231112
Kayin Miranda <kayin.edianne.miranda@student.ateneo.edu>
2026-02-19 16:03:35 +0800

    made recipe, ingridient and recipe ingridient models

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('recipe', args=[str(self.pk)])
+    
+class Recipe(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('recipe', args=[str(self.pk)])
+
+class RecipeIngridient(models.Model):
+    quantity = models.IntegerField()
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name='ingredients')
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='recipes')
+    
+    class Meta: 
+        ordering = ['recipe']
+        verbose_name = 'recipeingridient'
+        verbose_name_plural = 'recipeingridients'
